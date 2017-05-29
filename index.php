86ef10b64c38d3e12a67480da48317e108b9d14e
Wolf117Warrior <wolf117warrior@openmailbox.org>
2017-05-29 17:54:59 +0200

    Correction des bug de style
    Correction des bug de style

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<?php      
+<?php
 //=========================================================
 // conexion Bdd
 //=========================================================
-include("./config/ConnexionBdd.php"); 
-include("./config/fonctions.php"); 
-// fuseau horaire 
-date_default_timezone_set('America/Martinique'); 
+include("./config/ConnexionBdd.php");
+include("./config/fonctions.php");
+// fuseau horaire
+date_default_timezone_set('America/Martinique');
 ?>
 <!DOCTYPE HTML>
 <!--
@@ -15,30 +15,27 @@ date_default_timezone_set('America/Martinique');
 -->
 <html>
 
-
 <!-- Head -->
+<?php include("./include/head.php"); ?>
 
-   <?php include("./include/head.php"); ?>
+<body>
 
-	<body>
-
-		<!-- LOGO --> <!-- Bannière -->
-			<section id="banner">
-			</section>
-
-
+  <!-- LOGO --> <!-- Bannière -->
+	 <section id="banner">
+	  </section>
 
 <!-- NAVBAR -->
-
-<header id="header">
+  <header id="header">
+    <div>
     <a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
-  </div>
+    </div>
 </header>
 
 <!-- NAVBAR -->
 <?php include("./include/navbar.php"); ?>
 
 <!-- Formulaire de recherche -->
+<section>
   <div id="recherche" class="inner">
     <form method="post" action="searchengine.php">
       <div class="row uniform recherche-container">
@@ -54,7 +51,7 @@ date_default_timezone_set('America/Martinique');
 </section>
 
 <section id="content">
-<h3>Accueil</h3>
+  <h3>Accueil</h3>
 <section id="main" class="article-wrapper">
 
   <div class="inner taille1">
@@ -63,22 +60,22 @@ date_default_timezone_set('America/Martinique');
 
   <?php //--------------------------------//
         //--- affichage liste articles ---//
-        //--------------------------------// 
+        //--------------------------------//
         $result=$maBase->query("SELECT a.*, c.LIBL_categorie  AS 'LIBL_categorie'
-                FROM cnamcp09_articles a LEFT JOIN cnamcp09_categories c ON a.ID_categorie = c.ID_categorie 
-                WHERE c.LIBL_categorie IS NOT NULL ORDER BY DATE_article DESC LIMIT 0,3"); 
+                FROM cnamcp09_articles a LEFT JOIN cnamcp09_categories c ON a.ID_categorie = c.ID_categorie
+                WHERE c.LIBL_categorie IS NOT NULL ORDER BY DATE_article DESC LIMIT 0,3");
             $i = 0;
             $count=$result->rowCount() ;
-                         if($result) { 
-                              while($article=$result->fetch()) { 
-                                $i++; 
+                         if($result) {
+                              while($article=$result->fetch()) {
+                                $i++;
                                 $article_id = $article['ID_article'];
                                 $art = html_entity_decode($article['TITRE_article']);
             ?>
     <?php if($i==1) { ?>
       <div class="12u$(small) taille2">
         <h2>Les derniers articles</h2>
-        
+
       </div>
     <?php } ?>
 <?php if($i==1||$i==2) { ?>
@@ -89,27 +86,27 @@ date_default_timezone_set('America/Martinique');
           <h4 class="cat">Catégorie : </h4>
           <p class="cat"><?php echo html_entity_decode($article['LIBL_categorie']); ?></p>
           <h3 class="titre"><?php echo html_entity_decode($article['TITRE_article']); ?></h3>
-          <?php 
+          <?php
                     $nom_img = formateNomImage(html_entity_decode($article['TITRE_article']));
                     $img_o = './medias/'.$article_id.'-'.$nom_img.'-o.jpg?v='.(file_exists('./medias/'.$article_id.'-'.$nom_img.'-o.jpg')?filemtime('./medias/'.$article_id.'-'.$nom_img.'-o.jpg'):'');
                     $img_m = './medias/'.$article_id.'-'.$nom_img.'-m.jpg?v='.(file_exists('./medias/'.$article_id.'-'.$nom_img.'-m.jpg')?filemtime('./medias/'.$article_id.'-'.$nom_img.'-m.jpg'):'');
                     $img_p = './medias/'.$article_id.'-'.$nom_img.'-p.jpg?v='.(file_exists('./medias/'.$article_id.'-'.$nom_img.'-p.jpg')?filemtime('./medias/'.$article_id.'-'.$nom_img.'-p.jpg'):'');
-                    $photo = (file_exists('./medias/'.$article_id.'-'.$nom_img.'-o.jpg')); 
+                    $photo = (file_exists('./medias/'.$article_id.'-'.$nom_img.'-o.jpg'));
 
           ?>
           <?php echo date_format(new DateTime($article['DATE_article']), 'd/m/Y H:i:s'); ?>
           <span class="image fit"><img src="<?php echo ($photo?($i==1?$img_m:$img_p):'./images/no_pic.jpg'); ?>" alt=""></span>
-          
+
           <p>
             <?php echo tronqueTexte(html_entity_decode($article['CONTENT_article']),800); ?>
             <a href="article.php?id=<?php echo $article_id; ?>" class="button special small">Lire la suite</a>
           </p>
         </div>
- <?php if($i==1||$i==3) { ?>         
+ <?php if($i==1||$i==3) { ?>
       </div>
 <?php } ?>
-              <?php           } 
-                      }   
+              <?php           }
+                      }
                       if($count==0)  echo "<div class='row'>pas d'articles</div>";
               ?>
 
@@ -120,9 +117,9 @@ date_default_timezone_set('America/Martinique');
 
       <?php
       //============================================
-      //== Affichage encart liste des catégories ==  
+      //== Affichage encart liste des catégories ==
       //============================================
-          include("./include/categories.php"); 
+          include("./include/categories.php");
       ?>
 
         <div class="inner main-encart2 portfolio">
