9d3fa297ae33176c5350d7c1775956a3417241f7
rvmj <herve.marie-joseph@orange.fr>
2017-06-03 22:13:06 -0400

    update wyziwig tinymce

--- include/head.php
+++ include/head.php
@@ -21,5 +21,28 @@
       $link_js = (isset($access)&&$access=='admin'?'../':'').'assets/js/validationFormulaire.js';
       $link_js .= '?v='.filemtime($link_js);  ?>
       <script type="text/javascript" src="<?php echo $link_js; ?>"></script>
+      <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=d9gev28oiz7icwy2ev18240rkedm4esg5rpdes2c25msxce5"></script>
+      <script>tinymce.init({ selector:'textarea' ,
+        height: 500,
+        theme: 'modern',
+        plugins: [
+          'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+          'searchreplace wordcount visualblocks visualchars code fullscreen',
+          'insertdatetime media nonbreaking save table contextmenu directionality',
+          'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc help'
+        ],
+        toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
+        image_advtab: true,
+        templates: [
+          { title: 'Test template 1', content: 'Test 1' },
+          { title: 'Test template 2', content: 'Test 2' }
+        ],
+        content_css: [
+          '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+          '//www.tinymce.com/css/codepen.min.css'
+        ]
+       });</script>
   <?php } ?>
+
 </head>
