fe502d5c19b918589f0ba228eb8432abeb1a094c
Wolf117Warrior <wolf117warrior@openmailbox.org>
2017-06-20 20:16:44 +0200

    Ajout de l'affichage user
    Affichage des candidats CNAM et leur nfa

--- include/footer.php
+++ include/footer.php
@@ -4,9 +4,37 @@
       <div class="copyright">
         &copy; Source du design : <a href="https://templated.co">TEMPLATED</a>
       </div>
+
       <div>
+
+        <!-- messages -->
+        <section id="one">
+
+          <div>
+
+          <?php
+            //--------------------------------//
+            //--- affichage des candidat et le nfa ---//
+            //--------------------------------//
+
+              $result=$maBase->query("SELECT ID_utilisateur, NOM_utilisateur, COURS_nfa FROM cnamcp09_utilisateurs WHERE COURS_nfa='nfa021' ");
+                if($result) {
+                  while($user=$result->fetch()) {
+                    $user_id = $user['ID_utilisateur'];
+                    $user_nom = html_entity_decode($user['NOM_utilisateur']);?>
+
+                <b><?php echo $user_nom; ?></b>
+                <?php echo html_entity_decode($user['COURS_nfa']); ?>
+          <?php           }
+                    }
+          ?>
+         </div>
+        </section>
+    </div>
+
+    <div>
         <a href="../mention.php">Mentions légales</a>
-      </div>
+    </div>
 
     </div>
   </div>
